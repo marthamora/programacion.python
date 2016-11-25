5123a015255215dbe7ed5c8169d2b44cf1afbd52
marthamora <marthamora1235@gmail.com>
2016-11-24 23:12:58 -0600

    Update eejercicio5.py

--- marthatarea1/eejercicio5.py
+++ marthatarea1/eejercicio5.py
@@ -3,3 +3,16 @@
 #laboratorio1
 
 
+xs=1
+xm=60*xs
+xh=60*xm
+xd=24*xh
+print "ingrese el numero de dias"
+d = float(raw_imput())
+print "ingrese el numero de horas"
+h = float(raw_imput())
+print "ingrese el numero de minutos"
+m = float(raw_imput())
+print "ingrese el numero de segundos"
+s = float(raw_imput())
+print (xs*s)+(m*xm)+(xh*h)+(xd*d),"segundos"
