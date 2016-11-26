40187862a9572fc079872bed5d8d010b85d169bc
marthamora <marthamora1235@gmail.com>
2016-11-25 23:57:47 -0600

    Delete tarea22.py

--- tarea2/tarea22.py
+++
@@ -1,7 +0,0 @@
-#Martha Elena Nieves Mora
-#25112016
-#laboratorio 1, tarea 2x=input("distancia recorrida en km")
-def cantidadapagar(x):
-    y=8.74+1.84*4*x
-    return y
-print "la cantidadapagar es:",cantidadapagar(x)
