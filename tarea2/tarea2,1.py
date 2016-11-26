a36f6a552d6c2b9215f6d390c9d20b0a02c35570
marthamora <marthamora1235@gmail.com>
2016-11-25 23:07:47 -0600

    tarea 2
    laboratorio 1

---
+++ tarea2/tarea2,1.py
@@ -0,0 +1,9 @@
+#Martha Elena Nieves Mora
+#25112016
+#laboratorio 1, tarea 2x=input("valor cateto opuesto")
+y=input("cateto adyacente")
+import math
+def hip(x,y):
+    i=math.sqrt(x*x+y*y)
+    return i
+print "la hip es:", hip(x,y)
---
+++ tarea2/tarea2123.py
@@ -0,0 +1,29 @@
+#Martha Elena Nieves Mora
+#25112016
+#laboratorio 1, tarea 2
+x=input("numero entero n")
+def poligono(x):
+    return x
+import matplotlib.pyplot as plt
+listahex=[[4,5,6,5,4,3,4],[1,1,2,3,3,2,1]]
+plt.plot(listahex)
+plt.plot(listahex[0],listahex[1])
+plt.show()
+
+import matplotlib.pyplot as plt
+listacua=[[1,1,0,0],[0,1,1,0]]
+plt.plot(listacua)
+plt.plot(listacua[0],listacua[1])
+plt.show()
+
+listapent=[[2,3,3.5,2.5,1.5,2],[1,1,2,3,2,1]]
+plt.plot(listapent)
+plt.plot(listapent[0],listapent[1])
+plt.show()
+
+import matplotlib.pyplot as plt
+listaocta=[[4,5,6,6,5,4,3,3,4],[1,1,2,3,4,4,3,2,1]]
+plt.plot(listaocta)
+plt.plot(listaocta[0],listaocta[1])
+plt.show()
+print x
