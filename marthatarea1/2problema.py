240b449b1bfcbf0fb83ae9b640deb86e40c39b0d
marthamora <marthamora1235@gmail.com>
2017-01-31 08:38:18 -0600

    Update 2problema.py

--- marthatarea1/2problema.py
+++ marthatarea1/2problema.py
@@ -1,26 +1,26 @@
 #Nieves Mora Martha Elena
 #24112016
 #laboratorio1
-def intereses(n):
+def intereses(x):
     sum=0
-    y=(n*.04)
-    sum=n+y
+    i=(x*0.04)
+    sum=x+i
     return sum
-n=input()
-print intereses(n)
-
-def intereses(n):
+x = input()
+print "en 1 año tu saldo sera:", intereses(x)
+print "ingresa nuevamente tu saldo inicial"
+def intereses(x):
     sum=0
-    y=(2n*.04)
-    sum=n+y
+    i=(2*x*0.04)
+    sum=x+i
     return sum
-n=input()
-print intereses(n)
-
-def intereses(n):
+x = input()
+print "en 2 años tu saldo sera:", intereses(x)
+print "ingresa nuevamente tu saldo inicial"
+def intereses(x):
     sum=0
-    y=(3n*.04)
-    sum=n+y
+    i=(3*x*0.04)
+    sum=x+i
     return sum
-n=input()
-print intereses(n)
+x = input()
+print "en 3 años tu saldo sera:", intereses(x)
