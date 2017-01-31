82d2f1b8472e5fc201b30c53cbd3ef4eb43c5ba1
marthamora <marthamora1235@gmail.com>
2017-01-31 08:52:55 -0600

    Update tarea3.py

--- tarea3/tarea3.py
+++ tarea3/tarea3.py
@@ -1,11 +1,16 @@
 #martha elena nieves mora
 #laboratorio 1 tarea 3
 #29112016
-x = raw_input("introduzca una edad")
-
-if x == x.lower():
-    if x in range(0,3)
-    print "10.5"
-else:
-    if x in range(3,x+1)
-    print "4 años perro"
+	
+ap=input("ingresa los años-persona: ")
+if ap < 1:
+    print "error de entrada"
+elif ap == 1:
+    a=ap*10.5
+    print ap, "año-persona equivale a", a, "años-perro"
+elif ap == 2:
+    b=ap*10.5
+    print ap, "años-persona equivale a", b, "años-perro"
+elif ap > 2:
+    c=ap*4
+    print ap, "años-persona equivale a", c, "años-perro"
